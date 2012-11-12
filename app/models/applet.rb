7a1160b85b080c2ad4b03da992b3935beba2af3e
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-11-12 11:54:44 -0300

    Attr accessible

--- app/models/applet.rb
+++ app/models/applet.rb
@@ -1,3 +1,3 @@
 class Applet < ActiveRecord::Base
-  attr_accessible :content, :javascript, :style, :title
+  attr_accessible "title", "content", "style", "javascript", "objective", "abstract", "curriculum", "theme", "author", "language", "country", "publisher", "description", "rightsholder", "submiter", "address", "license", "uri", "other_links"
 end
